74b5fd44ad2f13a8427b62f5871c30222e9899b5
Nathan Craike <ncraike@gmail.com>
2015-08-29 16:29:38 +1000

    Renames tests to consistently use "should" convention.

--- tests/unit/test_dependency_register.py
+++ tests/unit/test_dependency_register.py
@@ -2,19 +2,19 @@
 
 class Test_DependencyRegister__construction:
 
-    def test__on_new_dep_reg__dependents_should_be_empty(self, dep_reg):
+    def test__after_creation__dependents_should_be_empty(self, dep_reg):
         assert len(dep_reg.dependents) == 0
 
-    def test__on_new_dep_reg__resources_should_be_empty(self, dep_reg):
+    def test__after_creation__resources_should_be_empty(self, dep_reg):
         assert len(dep_reg.resources) == 0
 
 class Test_DependencyRegister__register_dependent:
 
-    def test__can_call_with_dependent_and_resource_name(
+    def test__giving_dependent_and_resource_name__should_succeed(
             self, dep_reg, fake_resource_name, fake_dependent):
         dep_reg._register_dependent(fake_dependent, fake_resource_name)
 
-    def test__if_dependent_is_not_in_dependents_calling_adds_it(
+    def test__giving_dependent_not_in_dependents__should_add_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_dependent is not in dep_reg.dependents
         dep_reg.dependents.pop(fake_dependent, None)
@@ -23,7 +23,7 @@ class Test_DependencyRegister__register_dependent:
 
         assert fake_dependent in dep_reg.dependents
 
-    def test__if_dependent_is_not_in_dependents_calling_adds_resource_name(
+    def test__giving_dependent_not_in_dependents__should_add_resource_name(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_dependent is not in dep_reg.dependents
         dep_reg.dependents.pop(fake_dependent, None)
@@ -32,7 +32,7 @@ class Test_DependencyRegister__register_dependent:
 
         assert fake_resource_name in dep_reg.dependents[fake_dependent]
 
-    def test__if_dependent_is_in_dependents_calling_adds_resource_name(
+    def test__giving_dependent_in_dependents__should_add_resource_name(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_dependent is in dep_reg.dependents
         dep_reg.dependents[fake_dependent] = []
@@ -43,12 +43,12 @@ class Test_DependencyRegister__register_dependent:
 
 class Test_DependencyRegister__register_resource_dependency:
 
-    def test__can_call_with_resource_name_and_dependent(
+    def test__giving_resource_name_and_dependent__should_succeed(
             self, dep_reg, fake_resource_name, fake_dependent):
         dep_reg._register_resource_dependency(
                 fake_resource_name, fake_dependent)
 
-    def test__if_resource_name_not_in_resources_calling_adds_it(
+    def test__giving_resource_name_not_in_resources__should_add_resource_name(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_resource_name is not in dep_reg.resources
         dep_reg.resources.pop(fake_resource_name, None)
@@ -58,7 +58,7 @@ class Test_DependencyRegister__register_resource_dependency:
 
         assert fake_resource_name in dep_reg.resources
 
-    def test__if_resource_name_is_not_in_resources_calling_adds_dependent(
+    def test__giving_resource_name_not_in_resources__should_add_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_resource_name is not in dep_reg.resources
         dep_reg.resources.pop(fake_resource_name, None)
@@ -68,7 +68,7 @@ class Test_DependencyRegister__register_resource_dependency:
 
         assert fake_dependent in dep_reg.resources[fake_resource_name]
 
-    def test__if_resource_name_is_in_resources_calling_adds_dependent(
+    def test__giving_resource_name_in_resources__should_add_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         # Ensure that fake_resource_name is not in dep_reg.resources
         dep_reg.resources[fake_resource_name] = set()
--- tests/unit/test_dependency_register__public_api.py
+++ tests/unit/test_dependency_register__public_api.py
@@ -3,16 +3,16 @@ import functools
 
 class Test_DependencyRegister__construction__public_api:
 
-    def test__new_dep_reg_can_be_created(self, dep_reg):
+    def test__creating_new__should_succeed(self, dep_reg):
         pass
 
 class Test_DependencyRegister_register__public_api:
 
-    def test__can_call_with_just_resource_name(
+    def test__giving_only_resource_name__should_succeed(
             self, dep_reg, fake_resource_name):
         dep_reg.register(fake_resource_name)
 
-    def test__calling_with_just_resource_name_should_give_partial(
+    def test__giving_only_resource_name__should_give_partial(
             self, dep_reg, fake_resource_name):
 
         result = dep_reg.register(fake_resource_name)
@@ -22,11 +22,11 @@ class Test_DependencyRegister_register__public_api:
         assert result.func == dep_reg.register
         assert result.args == (fake_resource_name,)
 
-    def test__can_call_with_resource_name_and_dependent(
+    def test__giving_resource_name_and_dependent__should_succeed(
             self, dep_reg, fake_resource_name, fake_dependent):
         dep_reg.register(fake_resource_name, fake_dependent)
 
-    def test__calling_with_dependent_should_return_dependent(
+    def test__giving_resource_name_and_dependent__should_return_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         '''
         When called with a resource name and a dependent, register()
@@ -37,7 +37,7 @@ class Test_DependencyRegister_register__public_api:
         result = dep_reg.register(fake_resource_name, fake_dependent)
         assert result == fake_dependent
 
-    def test__registering_new_dependent_should_add_dependent(
+    def test__registering_new_dependent__should_add_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         '''
         After calling register() with a resource name and dependent,
@@ -46,7 +46,7 @@ class Test_DependencyRegister_register__public_api:
         dep_reg.register(fake_resource_name, fake_dependent)
         assert fake_dependent in dep_reg.dependents
 
-    def test__registering_new_dependent_should_add_resource_name_under_dependent(
+    def test__registering_new_dependent__should_add_resource_name_under_dependent(
             self, dep_reg, fake_resource_name, fake_dependent):
         '''
         After calling register() with a resource name and dependent,
