83b8497bcae863d3a159a4c7ef55c5394a0b9c71
Nathan Craike <ncraike@gmail.com>
2015-12-24 22:28:55 +1000

    Tests: refactors BDD argument system to be used in other tests.

--- tests/unit_bdd/test_dependency_register_bdd.py
+++ tests/unit_bdd/test_dependency_register_bdd.py
@@ -8,6 +8,10 @@ from pytest_bdd import scenario, scenarios, given, when, then, parsers
 
 import fang.errors
 from utils import defers_when_steps
+from common.bdd.argument_system import (
+        argument_line,
+        get_argument_from_registered,
+        resolve_arg_lines)
 
 #
 # Module under test:
@@ -18,54 +22,6 @@ from fang.dependency_register import DependencyRegister
 # Load our test scenarios
 #
 scenarios('features/dependency_register/')
-
-arguments = []
-
-def give_argument_value_from_parsing_line(
-        argument_line, parser, get_argument_value_func,
-        **kwargs_for_arg_factories):
-    '''
-    This asks the given function for the argument value based on the
-    argument line.
-
-    The function is given the dictionary of matches from the
-    parser, which it may use to create the argument value.
-    '''
-    matches = parser.parse_arguments(argument_line)
-    kwargs_for_arg_factories.update(matches)
-    return get_argument_value_func(**kwargs_for_arg_factories)
-
-def register_argument(pattern_or_parser, get_argument_value_func):
-    parser = parsers.get_parser(pattern_or_parser)
-    give_argument_value = functools.partial(
-            give_argument_value_from_parsing_line,
-            parser=parser,
-            get_argument_value_func=get_argument_value_func)
-
-    argument_entry = (parser.is_matching, give_argument_value)
-    arguments.append(argument_entry)
-
-# For use as decorator
-def argument_line(pattern_or_parser):
-    def decorator(decorated_func):
-        register_argument(pattern_or_parser, decorated_func)
-        return decorated_func
-    return decorator
-
-def get_argument_from_registered(argument_line, **kwargs_for_arg_factories):
-    for (is_matching, give_argument_value) in arguments:
-        if is_matching(argument_line):
-            return give_argument_value(
-                    argument_line, **kwargs_for_arg_factories)
-    else:
-        raise Exception(
-                "Couldn't find a match for argument line: {!r}".format(
-                    argument_line))
-
-def parse_argument_lines(lines, **kwargs_for_arg_factories):
-    return [
-            get_argument_from_registered(line, **kwargs_for_arg_factories)
-            for line in lines]
 
 @pytest.fixture
 def world_state():
@@ -236,10 +192,6 @@ def given_testing_construction_of_DependencyRegister(
             "NOT APPLICABLE WHEN WE'RE TESTING INSTANCE CONSTRUCTION")
     world_state['instance'] = (
             "NOT INITIALISED YET")
-
-def resolve_arg_lines(lines, request):
-    return [get_argument_from_registered(line, pytest_request=request)
-            for line in lines.splitlines()]
 
 @given('I am ignoring all exceptions during the method call')
 def ignoring_exceptions_during_method_call(call_under_test):
