b93cd8f443e1ca61b14000e6b2e3de92af0c2bf9
Nathan Craike <ncraike@gmail.com>
2016-04-25 15:17:02 +1000

    Adds first unit test for ResourceProviderRegister.register()

--- tests/unit/test_resource_provider_register.py
+++ tests/unit/test_resource_provider_register.py
@@ -1,7 +1,22 @@
 
+import functools
+import unittest.mock
+import pytest
 
 # Class under test:
 from fang.resource_provider_register import ResourceProviderRegister
+
+@pytest.fixture()
+def mock_instance():
+    mock_instance = unittest.mock.NonCallableMock(
+            spec=ResourceProviderRegister())
+    mock_instance.dependents = {}
+    mock_instance.resources = {}
+    return mock_instance
+
+@pytest.fixture()
+def resource_name():
+    return 'test.resource'
 
 class Test_ResourceProviderRegister__construction:
 
@@ -39,3 +54,22 @@ class Test_ResourceProviderRegister_clear:
 
         instance.clear()
         assert len(instance.resource_providers) == 0
+
+class Test_ResourceProviderRegister_register:
+
+    def test__giving_no_provider_arg___should_return_partial(
+            self, mock_instance, resource_name):
+        '''
+        If register() is called with resource_name but no provider,
+        register() should return a partial of register() with the
+        resource_name argument fixed.
+
+        NOTE: In this test we call the method on the class, not an
+        instance, so we can give a mock instance for self.
+        '''
+        result = ResourceProviderRegister.register(
+                mock_instance, resource_name)
+
+        assert isinstance(result, functools.partial)
+        assert result.func == mock_instance.register
+        assert result.args == (resource_name,)
