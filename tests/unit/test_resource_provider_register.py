b513e4a82d93fd185fae65f17e2f707a807dbf51
Nathan Craike <ncraike@gmail.com>
2016-09-03 16:20:14 +1000

    Removes trailing whitespace in unit test
    Trailing whitespace was removed from the unit tests for ResourceProviderRegister

--- tests/unit/test_resource_provider_register.py
+++ tests/unit/test_resource_provider_register.py
@@ -45,11 +45,11 @@ class Test_ResourceProviderRegister__construction:
     def test__after_creation__namespace_should_default_to_None(self):
         instance = ResourceProviderRegister()
         assert instance.namespace is None
-    
+
     def test__after_creation_with_namespace__namespace_should_be_as_given(self):
         instance = ResourceProviderRegister('mynamespace')
         assert instance.namespace == 'mynamespace'
-        
+
     def test__after_creation__resource_providers_should_be_empty(self):
         instance = ResourceProviderRegister()
         assert len(instance.resource_providers) == 0
